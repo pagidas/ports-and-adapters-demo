77045fb0227a28e4e9ae91b5b23f087e204b27ae
pagidas <pagakrivos@gmail.com>
2021-08-19 10:37:41 +0100

    Tidy up

--- src/main/kotlin/org/example/demo/domain/port.kt
+++ src/main/kotlin/org/example/demo/domain/port.kt
@@ -5,6 +5,9 @@ package org.example.demo.domain
  */
 fun cardWalletLogicWithInMemoryRepository(): CardWalletPort = CardWalletLogic(repo = InMemoryCardWalletRepository())
 
+/**
+ * Primary/driving/input port that shapes how to communicate with the domain.
+ */
 interface CardWalletPort {
     fun createWallet(walletHolder: String): Wallet
     fun list(): List<Wallet>
@@ -12,6 +15,9 @@ interface CardWalletPort {
     fun getWalletById(id: WalletId): Wallet
 }
 
+/**
+ * Secondary/driven/output port that shapes how domain stores the state.
+ */
 interface CardWalletRepositoryPort {
     fun save(wallet: Wallet): Wallet
     fun getAll(): List<Wallet>
