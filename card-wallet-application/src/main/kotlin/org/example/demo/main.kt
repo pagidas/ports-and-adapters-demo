338e171b6033fcc828d86a6d9b9a7f6428b6f309
pagidas <pagakrivos@gmail.com>
2021-09-29 16:52:26 +0100

    Uses Koin config loader

--- card-wallet-application/src/main/kotlin/org/example/demo/main.kt
+++ card-wallet-application/src/main/kotlin/org/example/demo/main.kt
@@ -1,14 +1,20 @@
 package org.example.demo
 
 import org.koin.core.context.startKoin
+import org.koin.fileProperties
 
 fun main() {
-    // FIXME: 29/09/2021 Should come from a config
-    System.setProperty("MONGO_DB_URL", "mongodb://localhost:8080")
     val app: CardWalletApplication by lazy { CardWalletApplication() }
 
     startKoin {
         printLogger()
+        fileProperties("/application.properties")
+
+        // FIXME: 29/09/2021 Should be injected into nosql adapter.
+        val mongoDbUrl = koin.getProperty<String>("mongo_db_url")
+        println(mongoDbUrl)
+        System.setProperty("MONGO_DB_URL", mongoDbUrl)
+
         modules(module)
     }
 
