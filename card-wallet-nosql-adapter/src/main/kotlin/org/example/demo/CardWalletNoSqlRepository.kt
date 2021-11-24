ecf33422e4b5c66cb69d21de65a85f13a6260ab6
pagidas <pagakrivos@gmail.com>
2021-11-24 16:31:02 +0000

    Extracts mongodb client creation into a separate factory
    The creation of the mongodb client should not be the responsibility of
    the class that deals with the business logic.

--- card-wallet-nosql-adapter/src/main/kotlin/org/example/demo/CardWalletNoSqlRepository.kt
+++ card-wallet-nosql-adapter/src/main/kotlin/org/example/demo/CardWalletNoSqlRepository.kt
@@ -1,14 +1,16 @@
 package org.example.demo
 
 import com.mongodb.client.MongoCollection
-import org.litote.kmongo.*
+import org.litote.kmongo.eq
+import org.litote.kmongo.getCollection
+import org.litote.kmongo.updateOne
 
 class CardWalletNoSqlRepository(private val mongoDbConfig: MongoDbConfig): CardWalletRepositoryPort {
 
     private val walletsCol: MongoCollection<Wallet>
 
     init {
-        walletsCol = KMongo.createClient(mongoDbConfig.url()).run {
+        walletsCol = createMongoDbClient(mongoDbConfig).run {
             getDatabase(MongoDbConfig.CARD_WALLET_DB_NAME).getCollection<Wallet>(MongoDbConfig.WALLETS_COLLECTION_NAME)
         }
     }
@@ -30,15 +32,3 @@ class CardWalletNoSqlRepository(private val mongoDbConfig: MongoDbConfig): CardW
     }
 
 }
-
-data class MongoDbConfig(private val host: String = "localhost", private val port: Int) {
-    companion object {
-        private const val PROTOCOL = "mongodb"
-
-        const val CARD_WALLET_DB_NAME = "card_wallet"
-        const val WALLETS_COLLECTION_NAME = "wallets"
-    }
-
-    fun url(): String = "$PROTOCOL://$host:$port"
-}
-
