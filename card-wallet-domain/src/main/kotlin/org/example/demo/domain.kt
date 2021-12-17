20ab958a36dccc73b475fbc10d497a3d4c924f83
pagidas <pagakrivos@gmail.com>
2021-12-17 14:37:23 +0000

    Expand domain logic in conceptual objects
    All business logic of the service should be internal and encapsulated
    into domain objects.

---
+++ card-wallet-domain/src/main/kotlin/org/example/demo/domain.kt
@@ -0,0 +1,89 @@
+package org.example.demo
+
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import org.example.demo.WalletProblem.PassNotEnoughPoints
+import org.example.demo.WalletProblem.PassNotFound
+import java.util.*
+
+internal typealias PassesDomain = List<PassDomain>
+
+internal data class WalletIdDomain(val value: UUID) {
+    companion object {
+        fun random(): WalletIdDomain = WalletIdDomain(UUID.randomUUID())
+    }
+}
+internal data class PassIdDomain(val value: UUID)
+
+internal data class WalletDomain(val id: WalletIdDomain, val walletHolder: String, val passes: PassesDomain) {
+    companion object {
+        fun empty(id: UUID, walletHolder: String): WalletDomain = WalletDomain(WalletIdDomain(id), walletHolder, emptyList())
+    }
+
+    fun addPass(pass: PassDomain): WalletDomain? =
+        passes.find { it.id == pass.id }.let {
+            if (it == null) copy(passes = passes + pass) else null
+        }
+
+    fun debitPass(passId: PassIdDomain, amount: DebitAmount): Result4k<WalletDomain, WalletProblem> =
+        passes.find { it.id.value == passId.value }?.let { foundPass ->
+            foundPass.debit(amount)?.let { updatedPass ->
+                val newPasses = passes.toMutableList()
+                    .also {
+                        it.remove(foundPass)
+                        it.add(updatedPass)
+                    }
+                Success(copy(passes = newPasses))
+            } ?: Failure(PassNotEnoughPoints(passId, amount, foundPass.balance))
+        } ?: Failure(PassNotFound)
+}
+
+internal data class PassDomain(
+    val id: PassIdDomain,
+    val passName: String,
+    val passHolder: String,
+    val balance: PassBalance = PassBalance.Zero(),
+) {
+    fun debit(amount: DebitAmount): PassDomain? =
+        if (amount <= balance) copy(balance = balance - amount) else null
+}
+
+internal data class PassBalance(val value: Int) {
+    init {
+        require(value >= 0) { "PassBalance can not be negative" }
+    }
+
+    operator fun minus(amount: DebitAmount): PassBalance = copy(value = value - amount.value)
+
+    object Zero {
+        operator fun invoke(): PassBalance = PassBalance(0)
+    }
+}
+
+internal data class DebitAmount(val value: Int) {
+    init {
+        require(value > 0) { "DebitAmount can not be zero or negative" }
+    }
+
+    operator fun compareTo(balance: PassBalance): Int =
+        if (value == balance.value) 0
+        else if (value > balance.value) 1
+        else -1
+}
+
+internal sealed class WalletProblem {
+    data class PassNotEnoughPoints(
+        val passId: PassIdDomain,
+        val debitAmount: DebitAmount,
+        val balance: PassBalance,
+    ): WalletProblem() {
+        init {
+            require(debitAmount > balance) {
+                "Problem: $this can only happen when debit amount is greater than the pass balance"
+            }
+        }
+    }
+
+    object PassNotFound: WalletProblem()
+}
